904a730e449a030e6cf4ce45be83b09ddbb804da
tsl-sunjingang <tsl-sunjingang@tasly.com>
2022-08-10 09:24:10 +0800

    默认的

---
+++ webDevelop/src/main/java/com/example/webdevelop/WebDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDevelopApplication.class, args);
+	}
+
+}
---
+++ webDevelop/src/test/java/com/example/webdevelop/WebDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
